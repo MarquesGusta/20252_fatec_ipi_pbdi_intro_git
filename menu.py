c93375ff2b5a6ff8ea7638f751fcbea3e458e87a
MarquesGusta <gugamarquues2004@gmail.com>
2025-08-24 18:00:41 -0300

    feat(ap01-git): implementa a opcao da subtracao

--- menu.py
+++ menu.py
@@ -1,10 +1,18 @@
 import calculadora
 
-opcao = input("Selecione uma opção:\n1-Somar\n2-Subtrair\n3-Multiplicar\n4-Dividir\n0-Sair")
+def menu():
 
-if opcao == "1":
-     a = int(input("Informe o primeiro número: "))
-     b = int(input("Informe o segundo número: "))
+     opcao = input("1-Somar\n2-Subtrair\n3-Multiplicar\n4-Dividir\n0-Sair\n\nSelecione uma opção: ")
 
-     soma = calculadora.somar(a, b)
-     print(f"{a} + {b} = {soma}")
+     if opcao == "1":
+          a = int(input("Informe o primeiro número: "))
+          b = int(input("Informe o segundo número: "))
+
+          soma = calculadora.somar(a, b)
+          print(f"{a} + {b} = {soma}")
+     elif opcao == "2":
+          a = int(input("Informe o primeiro número: "))
+          b = int(input("Informe o segundo número: "))
+
+          subtracao = calculadora.subtrair(a, b)
+          print(f"{a} - {b} = {subtracao}")
